5c518c7622ce418cf0ba0842f5e26bfbef5dfb7f
joel-js <joel.js@inapp.com>
2023-07-02 16:51:52 +0530

    AxesHelper

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,62 +1,83 @@
-import * as THREE from "three";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xffffff)
+const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
 
-const camera1 = new THREE.PerspectiveCamera(
-  75,
-  1,
-  0.1,
-  1000
-);
-const camera2 = new THREE.OrthographicCamera(-2,2,2,-2,0.1, 100);
-camera1.position.z = 2;
-camera2.position.z = 2;
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.z = 2
 
-const canvas1 = document.getElementById('1') as HTMLCanvasElement;  
-const canvas2 = document.getElementById('2') as HTMLCanvasElement;  
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
-renderer1.setSize(200, 200);
+const controls = new OrbitControls(camera, renderer.domElement)
+//controls.addEventListener('change', render)
 
-const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
-renderer2.setSize(200, 200);
-
-const controls1 = new OrbitControls(camera1, canvas1);
-const controls2 = new OrbitControls(camera2, canvas2);
-
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
-  color: 0x0000ff,
-  wireframe: true,
-});
+    color: 0x00ff00,
+    wireframe: true,
+})
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const cube = new THREE.Mesh(geometry, material)
+cube.add(new THREE.AxesHelper(5));
+scene.add(cube)
 
-// window.addEventListener("resize", onWindowResize, false);
-// function onWindowResize() {
-//   camera1.aspect = window.innerWidth / window.innerHeight;
-//   camera1.updateProjectionMatrix();
-//   renderer1.setSize(window.innerWidth, window.innerHeight);
-//   render();
-// }
-
-function render() {
-  renderer1.render(scene, camera1);
-  renderer2.render(scene,camera2);
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
 }
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Cube')
+const cubeRotationFolder = cubeFolder.addFolder('Rotation')
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+cubeRotationFolder.open()
+const cubePositionFolder = cubeFolder.addFolder('Position')
+cubePositionFolder.add(cube.position, 'x', -10, 10, )
+cubePositionFolder.add(cube.position, 'y', -10, 10, )
+cubePositionFolder.add(cube.position, 'z', -10, 10, )
+cubeFolder.open()
+cubePositionFolder.open()
+const cubeScaleFolder = cubeFolder.addFolder('Scale')
+cubeScaleFolder.add(cube.scale, 'x', -5, 5)
+cubeScaleFolder.add(cube.scale, 'y', -5, 5)
+cubeScaleFolder.add(cube.scale, 'z', -5, 5)
+cubeFolder.add(cube, 'visible')
+cubeFolder.open()
+cubeScaleFolder.open()
 
 function animate() {
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate)
 
-  // cube.rotation.x += 0.01;
-  // cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01
+    //cube.rotation.y += 0.01
 
-  render();
-  controls1.update();
-  controls2.update();
+    render()
+
+    stats.update()
 }
 
-animate();
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
+//render()
