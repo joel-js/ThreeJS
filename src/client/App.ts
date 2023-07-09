9b009b1a00ee75acdbd8bb08d95d59834945d46e
joel.js <joel.js@inapp.com>
2023-07-09 21:00:01 +0530

    select wrapper

--- src/client/App.ts
+++ src/client/App.ts
@@ -1,13 +1,12 @@
 import * as THREE from 'three';
-import TransformControl from './Controls/TransformControl';
 import SceneInit from "./SceneInit";
 import { rt } from "./Utils/types";
 import MouseEvents from './Utils/MouseEvents';
 
 const App = (main: SceneInit, {meshes, wrappers}: rt) => {
-  const transformControl = TransformControl(main);
   const mouseEvents = new MouseEvents(main, { meshes, wrappers });
   mouseEvents.highLight();
+  mouseEvents.selectWrapper();
 }
  
 export default App;
