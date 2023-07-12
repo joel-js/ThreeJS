522b59ddf9e33330df70f64981ed795e19ae5ad3
joel.js <joel.js@inapp.com>
2023-07-12 10:51:13 +0530

    rotation done remaining tipping

--- src/client/Modules/TeethMovements.ts
+++ src/client/Modules/TeethMovements.ts
@@ -44,7 +44,6 @@ class TeethMovements {
     const name: string = wrapper.name;
     const list = new DoublyLinkedList(VectorMap);
     const { prev, next } = list.getPrevAndNext(name);
-    console.log("prev, next", { prev, next });
     const vector: WrapperLocalAxes = {
       prev: new THREE.Vector3(),
       next: new THREE.Vector3(),
@@ -55,54 +54,58 @@ class TeethMovements {
           this.wrappers[i].position,
           wrapper.position
         );
-        console.log("prev => ", wrapper.position, this.wrappers[i].position);
+        // console.log("prev => ", wrapper.position, this.wrappers[i].position);
       }
       if (next && this.wrappers[i].name === next) {
         vector.next = new THREE.Vector3().subVectors(
           this.wrappers[i].position,
           wrapper.position
         );
-        console.log("next => ", this.wrappers[i].position, wrapper.position);
+        // console.log("next => ", this.wrappers[i].position, wrapper.position);
       }
     }
     return vector;
   }
 
-  private mesial(wrapper: Wrapper) {
-    console.log("here@mesial", wrapper.position);
-    wrapper.position.add(this.findTranslateAxis(wrapper).next.multiplyScalar(0.1));
-    console.log("here@mesial2", wrapper.position);
+  private getLocalY(wrapper: Wrapper): THREE.Vector3 {
+    const worldY  = new THREE.Vector3(0, 1, 0);
+    const localY = worldY.applyQuaternion(wrapper.quaternion);
+    // console.log('world', worldY, 'local', localY  );
+    return localY;
   }
+
+  private mesial(wrapper: Wrapper) {
+    // console.log("here@mesial", wrapper.position);
+    wrapper.position.add(this.findTranslateAxis(wrapper).next.multiplyScalar(0.1));
+    // console.log("here@mesial2", wrapper.position);
+  }
+
   private distal(wrapper: Wrapper) {
-    console.log(
-      "here@distal",
-      wrapper.position,
-      wrapper.getWorldDirection(new THREE.Vector3(0, 0, 0))
-    );
+    // // console.log(
+    //   "here@distal",
+    //   wrapper.position,
+    //   wrapper.getWorldDirection(new THREE.Vector3(0, 0, 0))
+    // );
     this.findTranslateAxis(wrapper);
     wrapper.position.add(this.findTranslateAxis(wrapper).prev.multiplyScalar(0.1));
-    console.log("here@distal2", wrapper.position);
+    // console.log("here@distal2", wrapper.position);
   }
 
   private clockWise(wrapper: Wrapper, axes: WrapperLocalAxes) {
     const angle = Math.PI/18;
     const axis = axes.next.normalize();
-    // const quaternion = new THREE.Quaternion().setFromAxisAngle(axis, angle);
-    // wrapper.children[0].quaternion.multiplyQuaternions(quaternion, wrapper.children[0].quaternion);
+    // console.log('axis: ',axis);
     wrapper.rotateOnAxis(axis,angle);
 
   }
   private antiClockWise(wrapper: Wrapper, axes: WrapperLocalAxes){
-    const angle = -Math.PI/18;
-    const axis = axes.next.normalize();
-    // const quaternion = new THREE.Quaternion().setFromAxisAngle(axis, angle);
-    // wrapper.children[0].quaternion.multiplyQuaternions(quaternion, wrapper.children[0].quaternion);
+    const angle = -(Math.PI/12);
+    const axis = axes.prev.normalize();
     wrapper.rotateOnAxis(axis,angle);
   }
-
   private moveTeeth(wrapper: Wrapper) {
     if (this.keydownListener) {
-      console.log('x =>',this.keydownListener);
+      // console.log('x =>',this.keydownListener);
       window.removeEventListener("keydown", this.keydownListener);
     }
 
@@ -113,7 +116,7 @@ class TeethMovements {
           this.mesial(wrapper);
           break;
         case "s":
-          console.log(this.keyPressed)
+          // console.log(this.keyPressed)
           this.distal(wrapper);
           break;
         case "a":
@@ -122,6 +125,13 @@ class TeethMovements {
         case "d":
           this.antiClockWise(wrapper, this.findTranslateAxis(wrapper));
           break;
+        case "r":
+          this.clockWise(wrapper, { prev: new THREE.Vector3() ,next: this.getLocalY(wrapper)});
+          // wrapper.rotateY(Math.PI / 18);
+          break;
+        case "t":
+          this.antiClockWise(wrapper, { next: new THREE.Vector3() , prev: this.getLocalY(wrapper)});
+          // wrapper.rotateY(-Math.PI / 18)
       }
     };
     window.addEventListener("keydown", this.keydownListener);
@@ -144,9 +154,10 @@ class TeethMovements {
       const mesh = this.meshes[i];
       const wrapper = this.wrappers[i];
       if (this.intersectObject && this.intersectObject.name === mesh.name) {
-        console.log("@dbl", wrapper);
-        // const vertices = mesh.geometry.attributes.position.array;
-        // const faces = mesh.geometry.index?.array;
+        // console.log("@dbl", wrapper);
+        const vertices = mesh.geometry.attributes.position.array;
+        const faces = mesh.geometry.index?.array;
+        // console.log('faces', faces);
         this.moveTeeth(wrapper);
 
         break;
--- src/client/client.ts
+++ src/client/client.ts
@@ -18,16 +18,40 @@ const gumMaterial = new THREE.MeshBasicMaterial({ color: 0xff8080 });
 const meshes: Mesh[] = [];
 const meshWrappers: Wrapper[] = [];
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material2 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material2);
-client.scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material2 = [
+//   new THREE.MeshBasicMaterial({ color: 0xff0000 }), // Front face - red
+//   new THREE.MeshBasicMaterial({ color: 0x00ff00 }), // Back face - green
+//   new THREE.MeshBasicMaterial({ color: 0x0000ff }), // Top face - blue
+//   new THREE.MeshBasicMaterial({ color: 0xffff00 }), // Bottom face - yellow
+//   new THREE.MeshBasicMaterial({ color: 0xff00ff }), // Right face - magenta
+//   new THREE.MeshBasicMaterial({ color: 0x00ffff }), // Left face - cyan
+// ];
+// const mesh = new THREE.Mesh(geometry, material2);
+// client.scene.add(mesh);
+// const axis = new THREE.Vector3(0, 0, 1); // Y-axis
+// const angle = Math.PI / 4;
+// mesh.rotateOnAxis(axis, angle);
+// console.log('mesh', mesh)
+// // Get the local Y axis
+// const worldY = new THREE.Vector3(0, 1, 0); // Assuming Y is the local up direction
+// const localY = worldY.applyQuaternion(mesh.quaternion).normalize();
+// console.log(worldY);
+// const faceNormalsHelper = new THREE.FaceNormalsHelper(mesh, 1, 0x00ff00, 1);
+// const box = new THREE.Box3().setFromObject(mesh);
+// const center = box.getCenter(new THREE.Vector3());
+// const size = box.getSize(new THREE.Vector3());
+// const referenceVector = new THREE.Vector3(0, 1, 0); // Reference vector along the y-axis
+// const normal = new THREE.Vector3().crossVectors(size, referenceVector).normalize();
+// console.log(mesh);
+// console.log('Bounding Box Center:', center);
+// console.log('Bounding Box Size:', size);
+// console.log('Top Surface Normal:', normal);
 
-const axis = new THREE.Vector3(0, 1, 0); // Y-axis
-const angle = Math.PI;
-const quaternion = new THREE.Quaternion().setFromAxisAngle(axis, angle);
-mesh.quaternion.multiplyQuaternions(quaternion, mesh.quaternion);
-
+// const arrow = new THREE.ArrowHelper(localY);
+// arrow.setLength(10);
+// arrow.setColor(0xff00ff);
+// client.scene.add(arrow);
 
 plyLoader(files, meshes, meshWrappers, [material, gumMaterial])
   .then((result) => {
@@ -46,6 +70,5 @@ const animate = (): void => {
   client.controller.update();
   client.render();
   stats.update();
-  mesh.rotation.y += 0.01;
 };
 animate();
