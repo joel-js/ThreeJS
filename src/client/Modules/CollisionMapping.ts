88d7149537225aefbd947a69b428f0b3417fb5b4
joel.js <joel.js@inapp.com>
2023-07-20 13:34:08 +0530

    gui added

--- src/client/Modules/CollisionMapping.ts
+++ src/client/Modules/CollisionMapping.ts
@@ -11,15 +11,17 @@ import { setState } from "../State/MaterialState";
 // 0.2 > dist >= 0.0 : contact -> blue
 // 0.0 > dist >= -0.2 : inside -> pink
 // -0.2 > dist > -0.7: inside -> red
+
 class CollisionMapping {
   main: SceneInit;
   meshes: Mesh[];
   wrappers: Wrapper[];
-
+  gui: dat.GUI;
   constructor(main: SceneInit) {
     this.main = main;
     this.meshes = main.meshes;
     this.wrappers = main.wrappers;
+    this.gui = main.gui;
   }
 
   public execute() {
@@ -47,72 +49,83 @@ class CollisionMapping {
 
     const targetVector = getLocalY(wrapper); // Target vector to check parallelism
 
-    const geometry = mesh.geometry;
-    if (geometry.isBufferGeometry) {
-      const positionAttribute = geometry.attributes.position;
-      const normalAttribute = geometry.attributes.normal;
-      const indexArray = geometry.index?.array || [];
+    const controls = {
+      positionY: boxMesh.position.y,
+    };
 
-      if (positionAttribute && normalAttribute && indexArray) {
-        const positionArray: ArrayLike<number> = positionAttribute.array;
-        const vertexNormalArray: ArrayLike<number> = normalAttribute.array;
-        const vertices: Array<THREE.Vector3> = [];
-        const vertexNormals: Array<THREE.Vector3> = [];
-        for (let i = 0; i < positionArray.length; i += 3) {
-          vertices.push(
-            new THREE.Vector3(positionArray[i], positionArray[i + 1], positionArray[i + 2])
-          );
-          vertexNormals.push(
-            new THREE.Vector3(
-              vertexNormalArray[i],
-              vertexNormalArray[i + 1],
-              vertexNormalArray[i + 2]
-            )
+    this.gui.add(controls, 'positionY', 0, 12).onChange((val) => {
+      boxMesh.position.setY(val);
+    })
+    .onFinishChange(()=>{
+      boxMesh.updateMatrixWorld(true);
+      const geometry = mesh.geometry;
+      if (geometry.isBufferGeometry) {
+        const positionAttribute = geometry.attributes.position;
+        const normalAttribute = geometry.attributes.normal;
+        const indexArray = geometry.index?.array || [];
+  
+        if (positionAttribute && normalAttribute && indexArray) {
+          const positionArray: ArrayLike<number> = positionAttribute.array;
+          const vertexNormalArray: ArrayLike<number> = normalAttribute.array;
+          const vertices: Array<THREE.Vector3> = [];
+          const vertexNormals: Array<THREE.Vector3> = [];
+          for (let i = 0; i < positionArray.length; i += 3) {
+            vertices.push(
+              new THREE.Vector3(positionArray[i], positionArray[i + 1], positionArray[i + 2])
+            );
+            vertexNormals.push(
+              new THREE.Vector3(
+                vertexNormalArray[i],
+                vertexNormalArray[i + 1],
+                vertexNormalArray[i + 2]
+              )
+            );
+          }
+  
+          const raycaster = new THREE.Raycaster();
+          const colorArray = new Float32Array(positionAttribute.array.length).fill(1);
+          console.log("Len ", colorArray.length);
+  
+          vertexNormals.forEach((normal, i) => {
+            const dotProduct = normal.dot(targetVector.normalize());
+            if (dotProduct > 0.7) {
+              raycaster.set(vertices[i], normal.normalize());
+              const intersects: THREE.Intersection[] | undefined = raycaster.intersectObject(
+                boxMesh,
+                false
+                );
+                if (intersects.length) {
+                const dist: number = intersects[0].distance; 
+                  if (6 > dist && dist >= 4) {
+                    console.log(OccColorMap.green);
+                    colorArray.set(OccColorMap.green, i * 3);
+                  }
+                  else if( 4 > dist && dist >= 3) {
+                    colorArray.set(OccColorMap.cyan, i * 3);
+                  }
+                  else if( 3 > dist && dist >= 0 ) {
+                    colorArray.set(OccColorMap.red, i * 3);
+                  }
+              }
+            }
+          });
+  
+          const colorAttribute = new THREE.Float32BufferAttribute(colorArray, 3);
+          mesh.geometry.setAttribute("color", colorAttribute);
+          mesh.geometry.attributes.color.needsUpdate = true;
+          console.log('color',colorAttribute);
+          console.log(
+            "cc",
+            Array.from(colorArray).filter((color, i) => {
+              // if(color !== 0)
+              //   console.log(i)
+              return color !== 0;
+            })
           );
         }
-
-        const raycaster = new THREE.Raycaster();
-        const colorArray = new Float32Array(positionAttribute.array.length).fill(1);
-        console.log("Len ", colorArray.length);
-
-        vertexNormals.forEach((normal, i) => {
-          const dotProduct = normal.dot(targetVector.normalize());
-          if (dotProduct > 0.7) {
-            raycaster.set(vertices[i], normal.normalize());
-            const intersects: THREE.Intersection[] | undefined = raycaster.intersectObject(
-              boxMesh,
-              false
-              );
-              if (intersects.length) {
-              const dist: number = intersects[0].distance; 
-                if (6 > dist && dist >= 4) {
-                  console.log(OccColorMap.green);
-                  colorArray.set(OccColorMap.green, i * 3);
-                }
-                else if( 4 > dist && dist >= 3) {
-                  colorArray.set(OccColorMap.cyan, i * 3);
-                }
-                else if( 3 > dist && dist >= 0 ) {
-                  colorArray.set(OccColorMap.red, i * 3);
-                }
-            }
-          }
-        });
-
-        const colorAttribute = new THREE.Float32BufferAttribute(colorArray, 3);
-        mesh.geometry.setAttribute("color", colorAttribute);
-        mesh.geometry.attributes.color.needsUpdate = true;
-        console.log('color',colorAttribute);
-        console.log(
-          "cc",
-          Array.from(colorArray).filter((color, i) => {
-            // if(color !== 0)
-            //   console.log(i)
-            return color !== 0;
-          })
-        );
       }
-    }
+    })
+
   }
 }
 
