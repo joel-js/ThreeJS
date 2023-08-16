d43947b905c25cc1b3bf9e0548e3d519fb01996d
joel-js <joel.js@inapp.com>
2023-08-16 12:24:29 +0530

    run

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+
+
+
 import { Canvas } from "@react-three/fiber";
 import { usePlyLoader } from "./hooks/usePlyLoader";
-// import { OrbitControls } from '@react-three/drei'
 import { OrbitControls } from '@react-three/drei';
+
+const {spawn} = await import("child_process");
+
 
 const App = () => {
   const geometry = usePlyLoader("models/_gum.ply");
 
+
+const pythonProcess = spawn('python', ['./test.py']);
+pythonProcess.stdout.on('data', (data) => {
+  console.log("data");
+  
+  console.log(`Python Output: ${data}`);
+});
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas camera={{ fov: 75, position: [-10, 45, 20] }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
-        <mesh geometry={geometry} /> 
+        <mesh geometry={geometry}>
+        <meshBasicMaterial color={0xff8080} />
+        </mesh> 
         <OrbitControls dampingFactor={1}/>
       </Canvas>
     </div>
--- src/hooks/usePlyLoader.tsx
+++ src/hooks/usePlyLoader.tsx
@@ -1,8 +1,9 @@
 import { useLoader } from "@react-three/fiber";
 import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader.js";
 import { BufferGeometry } from "three"; // Import BufferGeometry from three.js
+// import React from 'react';
 
 export const usePlyLoader = (modelPath: string): BufferGeometry => {
-  const geometry = useLoader(PLYLoader, modelPath) as BufferGeometry;
+  const geometry: BufferGeometry = useLoader(PLYLoader, modelPath);
   return geometry;
 };
